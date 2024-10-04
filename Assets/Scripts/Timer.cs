26f7a1842f89f859ecd24c58969514a980e39d68
Julia Costa <juliacosta232005@gmail.com>
2024-10-04 01:40:46 +0100

    Fix Issues with WebCam

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,13 +8,19 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timertext;
     [SerializeField] private float time;
+    [SerializeField] private bool isInitializer;
+    [SerializeField] private GameObject otherTimer;
     private bool timerRun = true;
+    private float timerStore;
     private WinCheck win;
     private GameObject gameOverScreen;
+    private GameObject cameraManager;
     void Start()
     {
         win = FindFirstObjectByType<WinCheck>();
         gameOverScreen = FindFirstObjectByType<Lost>().gameObject;
+        cameraManager = FindFirstObjectByType<WebCamPhoto>().gameObject;
+        timerStore = time;
     }
     void Update()
     {
@@ -25,22 +31,38 @@ public class Timer : MonoBehaviour
             int seconds = (int) (time % 60) % 60;
             int minutes = (int) time / 60;
 
-            timertext.text = string.Format("{0:0}:{1:00}",minutes,seconds);
+            if (isInitializer)
+                timertext.text = string.Format("{0:0}",seconds);
+            else
+                timertext.text = string.Format("{0:0}:{1:00}",minutes,seconds);
 
             if (seconds == 0)
                 StopTimer();
         }
         else
         {
-            if (!win.HasWon)
+            if (isInitializer)
             {
-                Debug.Log("Time has run out!");
-                gameOverScreen.transform.GetChild(0).gameObject.SetActive(true);
+                cameraManager.GetComponent<WebCamPhoto>().PhotoTaker();
+                otherTimer.GetComponent<Timer>().RestartTimer();
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                if (!win.HasWon)
+                {
+                    Debug.Log("Time has run out!");
+                    gameOverScreen.transform.GetChild(0).gameObject.SetActive(true);
+                }
             }
         }
     }
     public void StopTimer()
     {
         timerRun = false;
+    }
+    public void RestartTimer()
+    {
+        time = timerStore;
     }
 }
--- Assets/Scripts/WebCamPhoto.cs
+++ Assets/Scripts/WebCamPhoto.cs
@@ -7,8 +7,7 @@ public class WebCamPhoto : MonoBehaviour
 {
     private WebCamTexture webCamTexture;
     [SerializeField] private GameObject[] allPieces = new GameObject[9];
-    Rect photoFrame = new Rect(0,0,720,720);
-    Vector2 photoPivot = new Vector2(600,240);
+    
     void Start()
     {
         webCamTexture = new WebCamTexture();
@@ -22,22 +21,42 @@ public class WebCamPhoto : MonoBehaviour
             StartCoroutine(TakePhoto());
         }
     }
+    public void PhotoTaker()
+    {
+        StartCoroutine(TakePhoto());
+    }
     IEnumerator TakePhoto()
     {
+        webCamTexture.Play();
         yield return new WaitForEndOfFrame();
 
         Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
         photo.SetPixels(webCamTexture.GetPixels());
         photo.Apply();
-        
-        byte[] bytes = photo.EncodeToPNG();
-        File.WriteAllBytes("Assets\\Sprites\\" + "photo.png", bytes);
-        Debug.Log($"Take Photo\nSave at: Assets\\Sprites\\photo.png");
 
-        
-        // Sprite Piece = Sprite.Create(photo,photoFrame,photoPivot);
+        webCamTexture.Stop();
+        // byte[] bytes = photo.EncodeToPNG();
+        // File.WriteAllBytes("Assets\\Sprites\\" + "photo.png", bytes);
+        // Debug.Log($"Take Photo\nSave at: Assets\\Sprites\\photo.png");
 
-        // allPieces[0].GetComponent<Image>().sprite = Piece;
+        Rect photoFrame = new Rect(360,480,240,240);
+        Vector2 photoPivot = new Vector2(120,120);
+
+        for (int i = 0; i < 9; i++)
+        {
+            Sprite Piece = Sprite.Create(photo,photoFrame,photoPivot);
+
+            allPieces[i].GetComponent<Image>().sprite = Piece;
+            // Debug.Log($"Set Piece {i} : Slice at ({photoFrame.x},{photoFrame.y})");
+
+            if ((i + 1) % 3 == 0)
+            {
+                photoFrame.x -= 480;
+                photoFrame.y -= 240;
+            }
+            else
+                photoFrame.x += 240;
+        }
 
     }
 }
